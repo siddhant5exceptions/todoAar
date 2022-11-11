144260575c8a90d0f7ba6cd4ecb1621da9495e28
Megha Raypuriya <megha.raypuriya@5exceptions.com>
2022-11-11 12:20:18 +0530

    add @Keep

--- aarSample/src/main/java/com/example/aarsample/ApiHandler.kt
+++ aarSample/src/main/java/com/example/aarsample/ApiHandler.kt
@@ -2,12 +2,14 @@ package com.example.aarsample
 
 import android.os.Handler
 import android.os.Looper
+import androidx.annotation.Keep
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.*
-
+@Keep
 class ApiHandler {
+    @Keep
     fun createSession() : String? {
         var response : String? = ""
         CoroutineScope(Dispatchers.IO + ErrorHandle().coroutineException()).launch {
