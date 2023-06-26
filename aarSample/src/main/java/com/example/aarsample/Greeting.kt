b9ea8bd6868ee61da362a4e528711d6ee5aed5b0
Akshay Mishra <akshay.mishra@5exceptions.com>
2023-06-26 19:24:52 +0530

    Changes in Greeting.kt

--- aarSample/src/main/java/com/example/aarsample/Greeting.kt
+++ aarSample/src/main/java/com/example/aarsample/Greeting.kt
@@ -3,12 +3,15 @@ package com.example.aarsample
 import android.app.Activity
 import android.content.Context
 import android.widget.Toast
+import androidx.annotation.Keep
 
+@Keep
 class Greeting {
+    @Keep
     fun generateGreetMessage(name: String): String {
         return "Welcome $name"
     }
-
+    @Keep
     fun showToast(mContext: Context, message: String) {
         Toast.makeText(mContext, message, Toast.LENGTH_LONG).show()
     }
